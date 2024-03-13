bfaa9ca685ca539a4ce1a81c3dace040c66b975c
wilson mutua <patiencechaltu@gmail.com>
2024-03-13 10:07:20 -0700

    loops

---
+++ 0x01-python-if_else_loops_functions/0-Positive-or-negative.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10, 10)
+if number > 0:
+    print(f"{number} is positive")
+elif number == 0:
+    print(f"{number} is zero")
+else:
+    print(f"{number} is negative")
+#!/usr/bin/python3
+import random
+number = random.randint(-10, 10)
+if number > 0:
+    print(f"{number} is positive")
+elif number == 0:
+    print(f"{number} is zero")
+else:
+    print(f"{number} is negative")
+
